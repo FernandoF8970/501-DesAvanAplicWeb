f5131717f6d5f0762e096397957685eeb336dba3
HJZR2004 <A01027743@tec.mx>
2025-05-30 17:55:04 -0600

    removed the buttons of the other classroom members

--- 501/vite-project/src/App.tsx
+++ 501/vite-project/src/App.tsx
@@ -18,33 +18,7 @@ function App() {
         <button onClick={() => window.location.href = '/A01028796/menu.html'}>
           A01028796
         </button>
-        <div>
-          <a href="/public/A01027913/Menu.html">
-            <button>Menu</button>
-          </a>
-        </div>
-        <a href="/public/A01028517/Menú.html"><button>Link menu A01028517</button></a>
-        <a href="/public/A01028517/Milestone2/Milestone2_Menu.html"><button>Link menu Milestone2</button></a>
-
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-        <a href="/src/class1/A01027983/">
-          <button>A01027983</button>
-        </a>
-        <a href='../src/class3/A01029143/pages/Menu.html'>
-          Menu A01029143
-        </a>
-      </div>
-      <a href="/public/A01028418/menu.html">
-        <button>A01028418</button>
-      </a>
     </>
   );
 }
